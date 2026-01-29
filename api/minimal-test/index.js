c8e2e3141f55f9a5b623278f942273113a02c42c
Jen <jeweigel@microsoft.com>
2026-01-28 21:31:22 -0800

    Fix Azure Functions runtime: add @azure/functions dependency, Node 18 + extension bundle v4, improved logging

--- api/minimal-test/index.js
+++ api/minimal-test/index.js
@@ -1,6 +1,11 @@
 module.exports = async function (context, req) {
+    context.log('Minimal test function executed');
+    
     context.res = {
         status: 200,
-        body: "OK"
+        headers: {
+            'Content-Type': 'text/plain'
+        },
+        body: 'OK - Minimal Test Working'
     };
 };
